66ed380ab6464d6ac43424771b01d850069c5166
Justin Naicker <128274515+Justin-Naicker@users.noreply.github.com>
2023-06-19 18:32:02 -0500

    Create Roman to Integer.java

---
+++ Roman to Integer.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> romanValues = new HashMap<>();
+        romanValues.put('I', 1);
+        romanValues.put('V', 5);
+        romanValues.put('X', 10);
+        romanValues.put('L', 50);
+        romanValues.put('C', 100);
+        romanValues.put('D', 500);
+        romanValues.put('M', 1000);
+
+        int result = 0;
+      //Length of string in chars
+        int n = s.length();
+
+      //Iterate through string
+        for (int i = 0; i < n; i++) {
+            if (i < n - 1 && romanValues.get(s.charAt(i)) < romanValues.get(s.charAt(i + 1))) {
+                result -= romanValues.get(s.charAt(i));
+            } else {
+                result += romanValues.get(s.charAt(i));
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.romanToInt("III"));
+        System.out.println(solution.romanToInt("LVIII"));   
+        System.out.println(solution.romanToInt("MCMXCIV"));  
+    }
