e24c19b1c53fc238eae2be66267bdaed630fc2da
Justin Naicker <128274515+Justin-Naicker@users.noreply.github.com>
2023-06-19 18:40:10 -0500

    Update Roman to Integer.java

--- Roman to Integer.java
+++ Roman to Integer.java
@@ -26,10 +26,3 @@ class Solution {
 
         return result;
     }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.romanToInt("III"));
-        System.out.println(solution.romanToInt("LVIII"));   
-        System.out.println(solution.romanToInt("MCMXCIV"));  
-    }
